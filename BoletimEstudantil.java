d110f2e05fa5b7394cee2b40944041dc755d037d
Jefferson Fernandes <68863695+Jefferson-AF@users.noreply.github.com>
2024-09-29 21:06:51 -0300

    Add files via upload

---
+++ BoletimEstudantil.java
@@ -0,0 +1,13 @@
+
+
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+       int mediaFinal = 7;
+       if(mediaFinal<6)
+       System.err.println("REPROVADO");
+       else if(mediaFinal==6)
+       System.err.println("PROVA MINERVA");
+       else 
+       System.err.println("APROVADO"); 
+    }
+}
